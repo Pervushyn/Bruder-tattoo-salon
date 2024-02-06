c3c10a4fe22cd801c2d1deb5d922e7a76dcded5b
Pervushyn <142146534+Pervushyn@users.noreply.github.com>
2024-02-06 17:15:26 +0100

    add

--- js/ascent.js
+++ js/ascent.js
@@ -1,20 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const fadeInElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, img, a'); // Вибираємо всі елементи для анімації
-  
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
+  const fadeInElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, img, a');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Додаємо затримку перед додаванням класу 'visible'
+        setTimeout(() => {
           entry.target.classList.add('visible');
-        } else {
-          entry.target.classList.remove('visible');
-        }
-      });
-    }, {
-      threshold: 0.1 // Конфігурація спостерігача, відсоток видимості елемента перед тим, як він стає "видимим"
+        }, 200); // 1000 мс затримки
+
+        // Відразу ж після активації анімації зупиняємо спостереження за цим елементом
+        observer.unobserve(entry.target);
+      }
     });
-  
-    fadeInElements.forEach(element => {
-      element.classList.add('fade-in'); // Додаємо клас для ефекту зникнення
-      observer.observe(element); // Починаємо спостерігати за кожним елементом
-    });
+  }, {
+    threshold: 0.1
   });
+
+  fadeInElements.forEach(element => {
+    element.classList.add('fade-in');
+    observer.observe(element);
+  });
+});
